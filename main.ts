ea197cf5581aadf437b3a98d267d97a957295c6a
Elliott-Roach <elliott.roach@mths.ca>
2024-10-23 10:49:24 -0400

    servo moving code

--- main.ts
+++ main.ts
@@ -5,4 +5,24 @@
  * This program turns a servo
 */
 
+const servoOne = robotbit.Servos.S1
 
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoOne, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoOne, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
