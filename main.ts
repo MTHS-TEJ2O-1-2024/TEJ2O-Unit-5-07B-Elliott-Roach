f819786f5b8c6e480fd8ec324bc7bb450403f97a
Elliott-Roach <elliott.roach@mths.ca>
2024-10-23 11:05:46 -0400

    add coments

--- main.ts
+++ main.ts
@@ -5,24 +5,24 @@
  * This program turns a servo
 */
 
+//setup
 const servoOne = robotbit.Servos.S1
-
 basic.showIcon(IconNames.Happy)
 
+//turns to 0 degrees
 input.onButtonPressed(Button.A, function () {
     robotbit.Servo(servoOne, 0)
     basic.clearScreen()
     basic.showString('0')
-    basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
 })
 
+//turns to 180 degrees
 input.onButtonPressed(Button.B, function () {
     robotbit.Servo(servoOne, 180)
     basic.clearScreen()
     basic.showString('180')
-    basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
 })
